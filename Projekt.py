604db189d08c7c61dc5b21246eb0bd50a6a2b020
Kamil <32820245+AstroSlazak@users.noreply.github.com>
2017-11-28 09:27:20 +0100

    Add files via upload

--- Projekt.py
+++ Projekt.py
@@ -10,8 +10,9 @@ from mpl_toolkits.basemap import Basemap
 import matplotlib
 import matplotlib.pyplot as plt
 
+# Funkcja do pobierania, zapisywania, i przetwarzania danych TLE satelitów
 def sat_position():
-    #sprawdzenie czy dane zostały już ściągnięte
+    # Sprawdzenie czy dane zostały już ściągnięte
     if os.path.exists('TLE_SATELLITE.txt'):
         data = open('TLE_SATELLITE.txt', 'r').readlines()
     else:
@@ -19,7 +20,7 @@ def sat_position():
         tle_url = 'http://www.celestrak.com/NORAD/elements/stations.txt'
         # Zwrócenie danych ze strony
         satellite_data = requests.get(tle_url).text
-        # zapis danych do pliku txt
+        # Zapis danych do pliku txt
         sat_data = open('TLE_SATELLITE.txt','w')
         sat_data.write(satellite_data)
         sat_data.close()
@@ -35,21 +36,25 @@ def sat_position():
     for line, value in enumerate(names[:-1], 1):
         x = str(line).strip()
         print(x, value)
-
+    # Wprowadzenie numeru satelity, i zwrócenie jej danych TLE
     number = input('Podaj numer satelity dla którego ma zostać wyznaczone położenie: ')
     sat_number = int(number) * 3 - 3
     return data[sat_number:sat_number+3]
 
+# Określenie pozycji obserwacji
 def my_position():
-    gmaps = googlemaps.Client(key='XXXXXXXXX')
+    # Klucz do googlemaps elevation API
+    key = open('key.txt', 'r').readlines()
+    gmaps = googlemaps.Client(key = key[0])
     x = input('Określić twoje aktualne położenie jako miejsce obserwacji ? [t/n] ')
-
+    # Jeżeli lokalizacja ma zostać wyznaczona dla aktualnej pozycji wyznacz się ją na podstawie IP
     if x.lower() == 't':
         g = geocoder.ip('me')
     else:
+        # Jeżeli dla dowolnego miejsca na podstwaie google
         y = str(input('Dla jakiego miejsca ma zostać wyznaczone położenie ? '))
         g = geocoder.google(y)
-
+    # Wyznaczenie długości, szerokości, przewyższenie
     lat = list(g.latlng)[0]
     lon = list(g.latlng)[1]
     e = gmaps.elevation(tuple(g.latlng))
@@ -57,19 +62,27 @@ def my_position():
     place = g.city
     return lat, lon, elev, place
 
+# Wywołanie funkcji pozycji satelity i podział danych
 data = sat_position()
 data1 = " ".join(data[0].split())
 data2 = data[1]
 data3 = data[2]
 
+# Wywołanie funcki własnej pozycji
 lat, lon, elev, place = my_position()
+
+# Określenie zmiennych dla obserwatora
 home = ephem.Observer()
 home.lat = lat
 home.lon = lon
 home.elevation = elev
+
+# Wprowadzenie danych orbity satelity
 sat = ephem.readtle(data1, data2, data3)
 
+# Określenie wielkości mapy
 matplotlib.rc('figure', figsize = (12, 6))
+# Określenie parametrów mapy
 m = Basemap(projection='kav7',
             lon_0 = 0,
             llcrnrlat = -90,
@@ -84,34 +97,49 @@ m.drawcountries(color = 'white',
                 zorder = 30)
 m.fillcontinents(color = '#1e1e1d',
                 lake_color = 'white')
+
+# Konwersja szerokości i długości geograficznej na parametry możliwe do wyświetlenia na mapie
 h_lon, h_lat = m(lon, lat)
+
+# Styl przedstawienia miejsca obserwacji na mapie
 m.plot(h_lon, h_lat,
         marker = '.',
         color = '#32caf6',
         markersize =6)
 
+# Deklaracja list dla szerokości i długości geograficznej satelity
 x_lon = []
 y_lat = []
 
+# Wypisanie położenia miejsca obserwacji
 print('\n')
 print('-' * 96)
 print('Miejsce obserwacji: %s | longitude: %5.2f deg | latitude: %5.2f deg | elevation %5.2f m ' % (place, lon, lat, elev))
 print('-' * 96)
 print('\n')
 
+# Pętla która oblicza położenie satelity w czasie rzeczywistym
 while True:
+    # Deklaracja czasu UTC
     home.date = datetime.utcnow()
+    # Obliczenia parametrów satelity
     sat.compute(home)
+    # Wypisanie danych
     print(data1 +" | altitude: %5.2f deg | azimuth %5.2f deg | longitude: %5.2f deg | latitude: %5.2f deg" % (math.degrees(sat.alt), math.degrees(sat.az), math.degrees(sat.sublong), math.degrees(sat.sublat)))
-
+    # Konwersja szerokości i długości geograficznej na parametry możliwe do wyświetlenia na mapie
     x, y = m(math.degrees(sat.sublong),math.degrees(sat.sublat))
+    # Dodanie parametrów satelity do listy
     x_lon.append(x)
     y_lat.append(y)
+    # Styl przedstawienia satelity na mapie
     m.plot(x_lon, y_lat, marker='_', markersize=2, color='r', linestyle='None', label=data1)
+    # Tytuł wykresu
     title = plt.title(data1 +' | longitude: %5.2f deg | latitude: %5.2f deg \nObserwator: %s | longitude: %5.2f deg | latitude: %5.2f deg ' % (math.degrees(sat.sublong), math.degrees(sat.sublat), place, lon, lat))
+    # Pozycja tytułu
+    title.set_y(1.01)
     plt.ion()
     plt.draw()
-    title.set_y(1.01)
     plt.show(block=False)
+    # Czasy przerw pomiędzy kolejnymi krokami pętli
     plt.pause(0.5)
     time.sleep(1.0)
